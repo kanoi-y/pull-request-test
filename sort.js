9ed04d34b5c7102255f7ec25580bd91ad6ebdfd7
kanoi-y <furafuratonbo@gmail.com>
2021-02-15 14:23:38 +0900

    コンソールログを消去

--- sort.js
+++ sort.js
@@ -1,4 +1,4 @@
-var sortNumber = function (number) {
+const sortNumber = function (number) {
     number.sort(function (a, b) {
         if (a === b) {
             return 0;
@@ -9,4 +9,3 @@ var sortNumber = function (number) {
  
  var number = [19, 3, 81, 1, 24, 21];
  sortNumber(number);
- console.log(number);
